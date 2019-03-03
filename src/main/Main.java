6f1960ba1fdfa1d2dbe2081f629a29e0d79c1a15
Katarzyna Jóźwiak <kasjozw@gmail.com>
2019-03-03 19:17:20 +0100

    add: threads executor

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,17 +1,30 @@
 package main;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
 
 
     public static void main(String[] args){
         VehicleFactory vehicleFactory = new VehicleFactory();
         long startTime = System.currentTimeMillis();
-        //String[] types = {"car", "motorcycle","truck","car","motorcycle"};
+
+        //String[] types = {"car", "motorcycle","truck","car","motorcycle"};  car-10, motor-5, truck-15
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
         String[] types = {"car", "car","car"};
         for(int i=0; i<types.length; i++){
-            MyThread thread = new MyThread(types[i]);
-            thread.run();
+            Runnable thread = new MyThread(types[i]);
+            //thread.run();
+            executor.execute(thread);
         }
+
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+        }
+        System.out.println("Finished all threads");
+
         long endTime = System.currentTimeMillis();
 
         long duration = (endTime - startTime) / 1000;
--- src/main/MyThread.java
+++ src/main/MyThread.java
@@ -9,9 +9,15 @@ class MyThread implements Runnable{
         this.vf = new VehicleFactory();
     }
 
+    @Override
     public void run(){
         Vehicle vehicle = vf.createVehicle(type);
-        vehicle.process();
+        //vehicle.process();
+        try {
+            Thread.sleep(10*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 }
